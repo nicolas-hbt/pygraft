db471135417ef255cb9a12a09b651e96d76f2e67
nicolas-hbt <nicolas.hubert@univ-lorraine.fr>
2023-09-07 09:45:58 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
   name = 'pygraft',
-  version = '0.1.2',
+  version = '0.1.2.1',
   packages = find_packages(),
   package_data={
         'pygraft': ['examples/template.json', 'examples/template.yml', 'property_checks/combinations.json', 'property_checks/compat_p1p2_inverseof.txt']
@@ -25,13 +25,13 @@ setup(
               'Semantic Web',
               'Synthetic Data Generator'],
   install_requires=[            
-          'art',
-          'numpy',
-          'Owlready2',
-          'rdflib',
-          'tabulate',
-          'tqdm',
-          'pyyaml'
+      'art>=6.0',
+      'matplotlib>=3.7',
+      'numpy>=1.24.2',
+      'Owlready2>=0.41',
+      'rdflib>=6.2.0',
+      'tabulate>=0.8.9',
+      'tqdm>=4.64.0'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',
